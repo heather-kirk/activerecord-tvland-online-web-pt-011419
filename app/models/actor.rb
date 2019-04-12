da8c1631ebff06439682aa6905823e7d59e969e1
Heather Kirk <hskirk@gmail.com>
2019-04-12 01:05:16 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,7 +9,6 @@ class Actor < ActiveRecord::Base
   end 
   
   def list_roles
-    binding.pry 
     roles = self.characters.map {|character| "#{character.name}, #{character.show.name}"}
   end 
 end
