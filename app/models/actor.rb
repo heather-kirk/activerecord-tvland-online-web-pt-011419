4b6366d3d7aed4a0e404ff52f205a1caaf9cd9a5
Heather Kirk <hskirk@gmail.com>
2019-04-12 00:57:32 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,13 +1,15 @@
+require 'pry'
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
   def full_name
+    binding.pry 
     "#{self.first_name} #{self.last_name}"
   end 
   
   def list_roles
     roles = Actor.map do {|character, show| character.name, show.name}
-  end 
+   end 
   end 
 end
